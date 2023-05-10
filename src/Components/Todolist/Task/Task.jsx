4c6b36f38bae49d40b2f0fb6c36d08ea10ae7ac5
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-10 20:03:31 +0300

    feat: <DragDropContext >     <Droppable>         <Draggable>
    it is not work yet, but chat will help to fix

--- src/Components/Todolist/Task/Task.jsx
+++ src/Components/Todolist/Task/Task.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import s from './Task.module.css'
+import { Draggable } from "react-beautiful-dnd";
 
 export const Task = (props) => {
 	return (
-		<li className={s.item}>
-			<div className={s.container}>
-				<div className={s.itemTitle}>{props.issueTitle}</div>
-				<div className={s.itemContent}>
-					<p>Task number # {props.issueNumber}</p>
-					<p>Task creation date: {props.issueCreationDate}</p>
-				</div>
-				<div className={s.itemInfo}>
-					<p>{props.issueOwner}</p>
-					<p>|</p>
-					<p>Comments: {props.commentsQuantity}</p>
-				</div>
-			</div>
-		</li>
+		<Draggable draggableId={props.issueTitle}
+			index={props.issueNumber}
+		>
+			{(provided) => (
+				<li className={s.item}
+					{...provided.draggableProps}
+					{...provided.dragHandleProps}
+					ref={provided.innerRef}
+				>
+					<div className={s.container}>
+						<div className={s.itemTitle}>{props.issueTitle}</div>
+						<div className={s.itemContent}>
+							<p>Task number # {props.issueNumber}</p>
+							<p>Task creation date: {props.issueCreationDate}</p>
+						</div>
+						<div className={s.itemInfo}>
+							<p>{props.issueOwner}</p>
+							<p>|</p>
+							<p>Comments: {props.commentsQuantity}</p>
+						</div>
+					</div>
+				</li>
+			)}
+		</Draggable>
 	)
 }
--- src/Components/Todolist/TodolistsItem/Todolist.jsx
+++ src/Components/Todolist/TodolistsItem/Todolist.jsx
@@ -1,30 +1,42 @@
 import React from "react";
 import s from './Todolist.module.css'
 import { Task } from "../Task/Task";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 export const Todolist = ({ title, tasks }) => {
-	//console.log(tasks);
+
+	const handleOnDragEnd = (result) => {
+		console.log(result);
+	}
 	return (
-		<div className={s.todolist}>
-			<div className={s.container}>
-				<h3 className={s.title}>{title}</h3>
-				<div className={s.body}>
-					<ul className={s.list}>
-						{tasks.length ? tasks.map((task) => {
-							//debugger
-							return <Task key={task.taskNumber}
-								issueTitle={task.title}
-								issueNumber={task.taskNumber}
-								issueCreationDate={task.created_at}
-								issueOwner={task.user}
-								commentsQuantity={task.comments}
-							/>
-						})
-							: <p>No Tasks</p>
-						}
-					</ul>
+		<DragDropContext onDragEnd={handleOnDragEnd} >
+			<div className={s.todolist}>
+				<div className={s.container}>
+					<h3 className={s.title}>{title}</h3>
+					<div className={s.body}>
+						<Droppable droppableId={title}>
+							{(provided) => (
+								<ul className={s.list}
+									{...provided.droppableProps}
+									ref={provided.innerRef}>
+									{tasks.length ? tasks.map((task) => {
+										return <Task key={task.taskNumber}
+											issueTitle={task.title}
+											issueNumber={task.taskNumber}
+											issueCreationDate={task.created_at}
+											issueOwner={task.user}
+											commentsQuantity={task.comments}
+										/>
+									})
+										: <p>No Tasks</p>
+									}
+									{provided.placeholder}
+								</ul>
+							)}
+						</Droppable>
+					</div>
 				</div>
 			</div>
-		</div>
+		</DragDropContext>
 	)
 }
