867f17173b2780a03f61f12aad2aca229ed8975b
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-12 17:46:59 +0300

    fix: remoove draggable and other dnd components

--- src/Components/Todolist/Task/Task.jsx
+++ src/Components/Todolist/Task/Task.jsx
@@ -4,29 +4,20 @@ import { Draggable } from "react-beautiful-dnd";
 
 export const Task = (props) => {
 	return (
-		<Draggable draggableId={props.issueTitle}
-			index={props.issueNumber}
+		<li className={s.item}
 		>
-			{(provided) => (
-				<li className={s.item}
-					{...provided.draggableProps}
-					{...provided.dragHandleProps}
-					ref={provided.innerRef}
-				>
-					<div className={s.container}>
-						<div className={s.itemTitle}>{props.issueTitle}</div>
-						<div className={s.itemContent}>
-							<p>Task number # {props.issueNumber}</p>
-							<p>Task creation date: {props.issueCreationDate}</p>
-						</div>
-						<div className={s.itemInfo}>
-							<p>{props.issueOwner}</p>
-							<p>|</p>
-							<p>Comments: {props.commentsQuantity}</p>
-						</div>
-					</div>
-				</li>
-			)}
-		</Draggable>
+			<div className={s.container}>
+				<div className={s.itemTitle}>{props.issueTitle}</div>
+				<div className={s.itemContent}>
+					<p>Task number # {props.issueNumber}</p>
+					<p>Task creation date: {props.issueCreationDate}</p>
+				</div>
+				<div className={s.itemInfo}>
+					<p>{props.issueOwner}</p>
+					<p>|</p>
+					<p>Comments: {props.commentsQuantity}</p>
+				</div>
+			</div>
+		</li>
 	)
 }
--- src/Components/Todolist/TodolistsItem/Todolist.jsx
+++ src/Components/Todolist/TodolistsItem/Todolist.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import s from './Todolist.module.css'
 import { Task } from "../Task/Task";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 export const Todolist = ({ title, tasks }) => {
 
@@ -9,34 +8,25 @@ export const Todolist = ({ title, tasks }) => {
 		console.log(result);
 	}
 	return (
-		<DragDropContext onDragEnd={handleOnDragEnd} >
-			<div className={s.todolist}>
-				<div className={s.container}>
-					<h3 className={s.title}>{title}</h3>
-					<div className={s.body}>
-						<Droppable droppableId={title}>
-							{(provided) => (
-								<ul className={s.list}
-									{...provided.droppableProps}
-									ref={provided.innerRef}>
-									{tasks.length ? tasks.map((task) => {
-										return <Task key={task.taskNumber}
-											issueTitle={task.title}
-											issueNumber={task.taskNumber}
-											issueCreationDate={task.created_at}
-											issueOwner={task.user}
-											commentsQuantity={task.comments}
-										/>
-									})
-										: <p>No Tasks</p>
-									}
-									{provided.placeholder}
-								</ul>
-							)}
-						</Droppable>
-					</div>
+		<div className={s.todolist}>
+			<div className={s.container}>
+				<h3 className={s.title}>{title}</h3>
+				<div className={s.body}>
+					<ul className={s.list}>
+						{tasks.length ? tasks.map((task) => {
+							return <Task key={task.taskNumber}
+								issueTitle={task.title}
+								issueNumber={task.taskNumber}
+								issueCreationDate={task.created_at}
+								issueOwner={task.user}
+								commentsQuantity={task.comments}
+							/>
+						})
+							: <p>No Tasks</p>
+						}
+					</ul>
 				</div>
 			</div>
-		</DragDropContext>
+		</div>
 	)
 }
