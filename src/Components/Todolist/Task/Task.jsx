25a7104cb66bb5038387e8cad0b7f7950d56da47
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-25 17:06:20 +0300

    начало использования useDrag и useDrop

--- src/Components/Todolist/Task/Task.jsx
+++ src/Components/Todolist/Task/Task.jsx
@@ -1,10 +1,28 @@
 import React, { useRef } from "react";
 import s from './Task.module.css'
+import { useDrag, useDrop } from "react-dnd";
 
 export const Task = ({ index, task, issueTitle, issueNumber, issueCreationDate, issueOwner, commentsQuantity }) => {
 
+	const ref = useRef(null)
+
+	const [{ }, drag] = useDrag({
+		type: 'card',
+		item: { index }
+	})
+
+	const [{ }, drop] = useDrop({
+		accept: 'card',
+		hover(item, monitor) {
+			console.log(item.index, index);
+		},
+		drop(item, monitor) {
+		}
+	})
+
+	drag(drop(ref))
 	return (
-		<li id={issueNumber} className={s.item}>
+		<li id={issueNumber} className={s.item} ref={ref}>
 			<div className={s.container}>
 				<div className={s.itemTitle}>{issueTitle}</div>
 				<div className={s.itemContent}>
